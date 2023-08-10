15dadefe29571766431d932cc5d4e32d8bf61b6a
Aman Rawat <56511200+amanrawat130@users.noreply.github.com>
2023-08-10 15:01:33 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,10 +17,9 @@ const router = express.Router();
 router.get("/:id", protectUserRoutes, grantAccessTo(["user", "admin", "superadmin"]), getUserByID);
 router.patch("/:id", protectUserRoutes, grantAccessTo(["user", "admin", "superadmin"]), updateUser);
 
+// Protect these routes with the protectAdminRoutes middleware
 router.post("/", grantAccessTo(["guest", "user", "admin", "superadmin"]), createUser);
-
-// Add a middleware to protect admin routes to GET and DELETE Routes below
-router.get("/", grantAccessTo(["admin", "superadmin"]), getAllUsers);
-router.delete("/:id", grantAccessTo(['superadmin']), deleteUser);
+router.get("/", protectAdminRoutes, grantAccessTo(["admin", "superadmin"]), getAllUsers);
+router.delete("/:id", protectAdminRoutes, grantAccessTo(['superadmin']), deleteUser);
 
 module.exports = router;
